31b11c19eeb9cd47263dfa0b0f8c706850362400
yasvisu <yasvisu@gmail.com>
2014-12-08 14:08:05 +0200

    Update gw2api.go

--- gw2api.go
+++ gw2api.go
@@ -1,8 +1,9 @@
 //Package gw2api provides bindings for the Guild Wars 2 API.
+//
 //Using this package is as simple as calling its functions.
 //
-//    func main() {
-//	    i := gw2api.Items()
-//		fmt.Println(i)
-//	}
+//   func main() {
+//     i := gw2api.Items()
+//     fmt.Println(i)
+//   }
 package gw2api
