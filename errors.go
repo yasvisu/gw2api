ba88eb5f4ecbf50e21eadd81689babb40d001966
Lennart Weller <lhw@ring0.de>
2016-01-29 11:54:35 +0100

    go vet complain fixed

--- errors.go
+++ errors.go
@@ -2,10 +2,10 @@ package gw2api
 
 //APIError for API errors to unmarshal into.
 type APIError struct {
-	err string `json:"error"`
+	Err string `json:"error"`
 }
 
 //Error implements the error interface.
 func (e APIError) Error() string {
-	return e.err
+	return e.Err
 }
